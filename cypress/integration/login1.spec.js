1f73598546fe7c71229f8d3102fb54d9253e7bf3
Vlad Pinaevsky <vladpinaevsky@MacBook-Air-Vlad.local>
2024-12-25 15:49:34 -0800

    2

--- cypress/integration/login1.spec.js
+++ cypress/integration/login1.spec.js
@@ -4,15 +4,15 @@ describe('Authorization Tests', () => {
     cy.visit('http://webtours.load-test.ru:1090/cgi-bin/nav.pl?in=home');
 
     // Ввод логина
-    cy.get('input[name="username"]').type('VladP'); // Замените 'VladP' на ваше имя пользователя
+    cy.get('input[name="username"]').type('VladP');
 
     // Ввод пароля
-    cy.get('input[name="password"]').type('1234'); // Замените '1234' на ваш пароль
+    cy.get('input[name="password"]').type('1234');
 
     // Нажатие на кнопку входа
-    cy.get('input[type="image"]').click(); // Кнопка входа
+    cy.get('input[type="image"]').click();
 
     // Проверка URL после входа
-    cy.url().should('include', 'nav.pl?in=home'); // Проверяем, что URL содержит нужную часть
+    cy.url().should('include', 'nav.pl?in=home');
   });
 });
